b7ea4c8b8110bdf820f37e49b4b7a883dc67db8e
026 <anbschool0026@026s-Mac-mini.local>
2023-10-20 19:51:02 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const morgan = require('morgan');
+const express = require('express');
+
+const tourRouter = require('./routes/tourRouter');
+const userRouter = require('./routes/userRoute');
+
+const app = express();
+
+// 1/ MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 3/ Routes
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
+module.exports = app;
---
+++ routes/tourRouter.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const tourController = require('./../controller/tourController');
+const router = express.Router();
+
+// router.param('id', tourController.checkID);
+router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
+router
+  .route('/top5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
+
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
+
+module.exports = router;
